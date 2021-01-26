6c0b395445b1951df895d0f7a34b2c75ef2954b7
Yan Burigo <yanburigoribeiro@gmail.com>
2021-01-26 19:43:19 -0300

    Mapa criado.

--- script.js
+++ script.js
@@ -10,6 +10,52 @@ var ang = 0;
 
 function desenhar() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  //mapa
+  ctx.fillStyle = "rgb(0, 128, 0)";
+  ctx.fillRect(0,0,640,480);
+  ctx.fillStyle = "rgb(0, 0, 0)";
+  ctx.beginPath();
+  ctx.arc(100, 380, 70, 0, 2 * Math.PI);
+  ctx.fill();
+  ctx.fillRect(90,370,450,80);
+  ctx.beginPath();
+  ctx.arc(550, 380, 70, 0, 2 * Math.PI);
+  ctx.fill();
+  ctx.fillRect(540,90,80,300);
+  ctx.beginPath();
+  ctx.arc(550, 90, 70, 0, 2 * Math.PI);
+  ctx.fill();
+  ctx.fillRect(350,20,200,80);
+  ctx.beginPath();
+  ctx.arc(350, 90, 70, 0, 2 * Math.PI);
+  ctx.fill();
+  ctx.fillRect(30,260,80,120);
+  ctx.beginPath();
+  ctx.arc(100, 250, 70, 0, 2 * Math.PI);
+  ctx.fill();
+  ctx.fillRect(90,180,210,80);
+  ctx.beginPath();
+  ctx.arc(291, 190, 70, 0, 2 * Math.PI);
+  ctx.fill();
+  ctx.fillRect(281,75,80,120);
+
+  ctx.fillStyle = "rgb(0, 128, 0)";
+  ctx.beginPath();
+  ctx.arc(470, 300, 70, 0, 2 * Math.PI);
+  ctx.arc(170, 315, 55, 0, 2 * Math.PI);
+  ctx.fill();
+  ctx.beginPath();
+  ctx.arc(480, 160, 60, 0, 2 * Math.PI);
+  ctx.arc(430, 170, 70, 0, 2 * Math.PI);
+  ctx.fill();
+  ctx.beginPath();
+  ctx.arc(211 , 110, 70, 0, 2 * Math.PI);
+  ctx.fill();
+
+  
+
+  //carro
+
   ctx.fillStyle = "rgb(255, 255, 255)";
   ctx.strokeStyle = "rgb(0, 255, 100)";
   ctx.lineWidth = 2;
@@ -55,16 +101,18 @@ function desenhar() {
 }
 requestAnimationFrame(desenhar);
 
+
 var movement = 0;
-var turn = 0;
+var turnLeft = 0;
+var turnRight = 0;
 
 document.onkeydown = function (evt) {
   switch (evt.keyCode) {
     case 39: //direita
-      turn = 39;
+      turnRight = 39;
       break;
     case 37: //esquerda
-      turn = 37;
+      turnLeft = 37;
       break;
     case 38: //cima
       movement = 38;
@@ -86,10 +134,10 @@ var intervalo = setInterval(() => {
 }, 10);
 
 var intervalo2 = setInterval(() => {
-  if (turn == 39 && movement != 0) {
-    ang += 0.07;
-  } else if (turn == 37 && movement != 0) {
-    ang -= 0.07;
+  if (turnRight == 39 && movement != 0) {
+    ang += 0.08;
+  } else if (turnLeft == 37 && movement != 0) {
+    ang -= 0.08;
   } 
 }, 10);
 
@@ -97,7 +145,10 @@ document.onkeyup = function (evt) {
   if (evt.keyCode == 38 || evt.keyCode == 40) {
     movement = 0;
   }
-  if (evt.keyCode == 37 || evt.keyCode == 39) {
-    turn = 0;
+  if (evt.keyCode == 37) {
+    turnLeft = 0;
+  }
+  if(evt.keyCode == 39){
+    turnRight = 0;
   }
 };
